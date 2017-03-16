af52d76d13e3d825f5f81216b0c69738e8270f70
Simon Boigelot <sboigelot@gmail.com>
2017-03-16 21:03:46 +0100

    Tileset collision box -> unity working

--- Assets/Maps/basetileset.tsx
+++ Assets/Maps/basetileset.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="basetileset" tilewidth="32" tileheight="32" tilecount="400" columns="20">
- <image source="Pyralis/gfx/gfx/tileset01.png" trans="000000" width="640" height="640"/>
+ <image source="tileset01.png" trans="000000" width="640" height="640"/>
+ <tile id="20">
+  <objectgroup draworder="index">
+   <object id="4" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
 </tileset>
