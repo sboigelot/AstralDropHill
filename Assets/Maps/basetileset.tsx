f2551436877071a58b8cc7262f47d489bd499e2c
Simon Boigelot <sboigelot@gmail.com>
2017-03-16 23:27:46 +0100

    Map collision update test

--- Assets/Maps/basetileset.tsx
+++ Assets/Maps/basetileset.tsx
@@ -1,9 +1,29 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="basetileset" tilewidth="32" tileheight="32" tilecount="400" columns="20">
  <image source="tileset01.png" trans="000000" width="640" height="640"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
  <tile id="20">
   <objectgroup draworder="index">
    <object id="4" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="269">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
 </tileset>
